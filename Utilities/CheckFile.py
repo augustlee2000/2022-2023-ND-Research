f6cade1f6fbea92463b50552c9940fb78a30ec94
August J. Lee <77298066+augustlee2000@users.noreply.github.com>
2023-06-05 13:32:46 -0400

    Update CheckFile.py

--- Utilities/CheckFile.py
+++ Utilities/CheckFile.py
@@ -36,10 +36,10 @@ def HardGet(e, L, H): # shorthand def for getting collection from event
 
 def GetDir(events,GL,GH):
     for event in events:
-        #the loop is only here because I am being super lazy and don't feel like figuring out how to get a single event from events
+     #if statement is only here because I am being super lazy and don't feel like remaking this event loop to go over only the first event
         event.getByLabel(GL,GH)
         GP = HardGet(event, GL, GH)
-        print(dir(GP[0]))
+        return dir(GP[0])
         break
 
 def Hist(events,GL,GH, variable,hist):
@@ -52,37 +52,51 @@ def Hist(events,GL,GH, variable,hist):
 files = glob.glob( sys.argv[1] ) #+"*root"
 events = Events(files)
 print(files)
-GH = Handle("vector<reco::GenParticle>") #handle from the edmDump
-GL = ("genParticles", "", "HLT") #lable from the edmDump
+Handle_string = input("Please input the Type: ")
+label_string_1 = input("Please input the Module: ")
+label_string_2 = input("Please input the Label: ")
+label_string_3 = input("Please input the Process: ")
+
+GH = Handle(Handle_string) #vector<Run3ScoutingPFJet>
+GL = (label_string_1, label_string_2, label_string_3) 
+
+yes_no = input("Whould you like the directory (y for yes; n for no): ")
+#This portion will get you all the variables that can be called!
+if yes_no =="y":
+    print(GetDir(events, GL, GH))
 
 
-#This portion will print you all the variables that can be called for the given handle and label!
-GetDir(events, GL, GH)
+histogram = input("whould you want a histogram? (y for yes; n for no)")
+
+if histogram == "y":
+    variable = input("What variable would you like the histogram of? ") 
+    #This is a very fast and dirty way to get a histogram from the file if you don't have access to a TBrowswer or justing being lazy
+    events = Events(files)
+
+    colors=[ROOT.kBlue,ROOT.kRed, ROOT.kBlack, ROOT.kGreen]
+    names = [variable]
+    x_starting = float(input("What is your minimium x value: "))
+    x_ending = float(input("What is your maximum x value: "))
+    bins = 500
+
+    # (number of histograms, colors array, name array, title, x axis, y axis, starting value, ending value, bins)
+    hist_array = MakeHist(1,colors,names,variable ,variable,"Instances",x_starting,x_ending,bins) 
 
 
+    #Event loop
+    for event in events:
+        GP = HardGet(event, GL,GH)
+        test = GetDir(events, GL, GH)
 
-#This is a very fast and dirty way to get a histogram from the file if you don't have access to a TBrowswer or justing being lazy
-events = Events(files)
+        for ig in GP:
+                value = getattr(ig,variable)()
+                hist_array[0].Fill(value)
 
-colors=[ROOT.kBlue,ROOT.kRed, ROOT.kBlack, ROOT.kGreen] #All of your basic histogram formating
-names = ["Eta"]
-x_starting = -6
-x_ending = 6
-bins = 500
+    #Drawing the histogram
+    c1=ROOT.TCanvas()
+    c1.Draw()
+    c1.cd()
+    hist_array[0].Draw("hist_same")
+    c1.Print("AOD_Variable_Output.png") #remember to change this if you are doing multiple runs
+    c1.cd()
 
-# (number of histograms, colors array, name array, title, x axis, y axis, starting value, ending value, bins)
-hist_array = MakeHist(1,colors,names,"Eta" ,"Eta","Instances",x_starting,x_ending,bins) 
-
-#Event loop
-for event in events:
-    GP = HardGet(event, GL,GH)
-    for ig in GP:
-            hist_array[0].Fill(ig.eta())
-
-#Drawing the histogram
-c1=ROOT.TCanvas()
-c1.Draw()
-c1.cd()
-hist_array[0].Draw("hist_same")
-c1.Print("AOD_Variable_Output.png") #remember to change this if you are doing multiple runs
-c1.cd()
